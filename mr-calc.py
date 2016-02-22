8b1828f1c131611f92052d78930379858e9f5328
Geoff-23 <lewispremington@gmail.com>
2016-02-22 21:41:12 +0000

    Hopeful fixed INT working for two letter elements

--- mr-calc.py
+++ mr-calc.py
@@ -4,19 +4,22 @@ formula= input("Test input: \n")
 fl = len(formula) +1
 fn=0
 while fn != fl:
-    if type(formula[fn]) == int:                #checks for numbers and adds extra elements
+    if formula[fn].islower() == True:    #checks if its an element with more than one letter
+        element = formula[fn,fn+1]
         chem_input[element] =+ 1
         fn += 1
+    elif formula[fn].isupper() == True:                                               #This is for single letter elements
+        element = formula[fn]
+        chem_input[element] =+ 1        #DOES'T WORK FOR 3 LETTER ELEMENTS
+        fn += 1                                      #E.G. Uup -
     else:
-        if formula[fn].islower() == True:    #checks if its an element with more than one letter
-            element = formula[fn,fn+1]
-            chem_input[element] =+ 1
+        if  formula[fn-1].islower() == True:
+            element = formula[fn-2,fn-1]
+            chem_input[element] =+ int(formula[fn])
             fn += 1
-        else:                                               #This is for single letter elements
-            element = formula[fn]
-            chem_input[element] =+ 1        #DOES'T WORK FOR 3 LETTER ELEMENTS
-            fn += 1                                      #E.G. Uup -
-
+        else:
+        element = formula[fn-1]
+        chem_input[element] =+ int(formula[fn])
 
 print(formula)
 print(chem_input)
