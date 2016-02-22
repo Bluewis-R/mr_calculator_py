e8fb7c2383d8cda55016dfe6e9ca99a01e520d62
Geoff-23 <lewispremington@gmail.com>
2016-02-22 21:11:38 +0000

    Added Input and started work on INT for elements

--- mr-calc.py
+++ mr-calc.py
@@ -1,5 +1,22 @@
-import math
-def carbon(num)
-    if num => 0
-        return
-    elif
+chem_input = {    'H':[0],}                     #The amout of chemical dictionary
+
+formula= input("Test input: \n")
+fl = len(formula) +1
+fn=0
+while fn != fl:
+    if type(formula[fn]) == int:                #checks for numbers and adds extra elements
+        chem_input[element] =+ 1
+        fn += 1
+    else:
+        if formula[fn].islower() == True:    #checks if its an element with more than one letter
+            element = formula[fn,fn+1]
+            chem_input[element] =+ 1
+            fn += 1
+        else:                                               #This is for single letter elements
+            element = formula[fn]
+            chem_input[element] =+ 1        #DOES'T WORK FOR 3 LETTER ELEMENTS
+            fn += 1                                      #E.G. Uup -
+
+
+print(formula)
+print(chem_input)
