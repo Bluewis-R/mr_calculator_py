efbd2a465ce2ca07314eff1c1c87ec793f80a1a2
Geoff-23 <lewispremington@gmail.com>
2016-02-23 20:06:52 +0000

    Code still broken but a lot neater

--- mr-calc.py
+++ mr-calc.py
@@ -1,25 +1,22 @@
-chem_input = {    'H':[0],}                     #The amout of chemical dictionary
-
-formula= input("Test input: \n")
+chem_table = {    'H':[0],}                    
+formula= input("Test input: (\n")
 fl = len(formula) +1
 fn=0
 while fn != fl:
-    if formula[fn].islower() == True:    #checks if its an element with more than one letter
-        element = formula[fn,fn +1]
-        chem_input[element] =+ 1
-        fn += 1
-    elif formula[fn].isupper() == True:                                               #This is for single letter elements
-        element = formula[fn]
-        chem_input[element] =+ 1        #DOES'T WORK FOR 3 LETTER ELEMENTS
-        fn += 1                                      #E.G. Uup -
-    else:
-        if  formula[fn-1].islower() == True:
-            element = formula[fn -2,fn -1]
-            chem_input[element] =+ int(formula[fn])
-            fn += 1
+    if formula[fn+1].islower() == True:
+        element = formula[fn:fn+1]
+        if formula[fn+2].isdigit() == True:
+            sub = formula[fn+2]
+            chem_table[element] =+ int(sub)
         else:
-            element = formula[fn -1]
-            chem_input[element] =+ int(formula[fn])
-
+            chem_table[element] =+ 1
+    elif formula[fn].isupper() == True:
+        element = formula[fn]
+        if formula[fn+1].isdigit() == True:
+            sub = formula[fn+1]
+            chem_table[element] =+ int(sub)
+        else:
+            chem_table[element] =+ 1
+print("TESTING")
 print(formula)
-print(chem_input)
+print(chem_table)
